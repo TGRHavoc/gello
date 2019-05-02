3008cc52a10b2b590c899ab14fc07cc43d46f5aa
Jordan Dalton <jordan.8474@gmail.com>
2019-05-02 12:01:51 +0100

    feat(boards): select boards
    One can now select a board by running the `board select` command.
    It takes an optional argument of either a board ID or a board name.
    If no arguments are supplied the user is given a choice to select one

--- commands/boards.go
+++ commands/boards.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/TGRHavoc/gello/translations"
 	"github.com/abiosoft/ishell"
 	"github.com/faith/color"
@@ -21,7 +24,64 @@ func InitCard(shell *ishell.Shell, translations *translations.Translation) {
 		Func: listBoards(translations),
 	})
 
+	board.AddCmd(&ishell.Cmd{
+		Name: "select",
+		Help: "Selects a board. If no ID/Name is provided then a menu will appear",
+		Func: selectBoard(translations, shell),
+	})
+
 	shell.AddCmd(board)
+}
+
+func selectBoard(translations *translations.Translation, shell *ishell.Shell) func(c *ishell.Context) {
+	return func(c *ishell.Context) {
+
+		var args = c.Args
+
+		boardIDSorted := translations.SortedBoardKeys()
+		shell.Set("selected_board", nil)
+
+		if len(args) == 0 {
+			var options []string
+			for _, id := range boardIDSorted {
+				board := translations.Boards[id]
+				options = append(options, fmt.Sprintf("%s\t%-4s", id, board.Name))
+			}
+
+			selectedOption := c.MultiChoice(options, "Select a board")
+
+			shell.Set("selected_board", translations.Boards[boardIDSorted[selectedOption]])
+			println("You have selected " + boardIDSorted[selectedOption])
+			return
+		}
+
+		if len(args) > 1 {
+			var newArgs = make([]string, 0)
+			newArgs = append(newArgs, strings.Join(c.Args, " "))
+			args = newArgs
+		}
+
+		// It's either an ID or a name...
+		if board, ok := translations.Boards[args[0]]; ok {
+			// It was an ID
+			shell.Set("selected_board", board)
+			c.Println("Selected " + board.Name)
+		} else {
+			// Check for names
+			for _, ID := range boardIDSorted {
+				board := translations.Boards[ID]
+				if strings.Compare(strings.ToLower(board.Name), strings.ToLower(args[0])) == 0 {
+					shell.Set("selected_board", board)
+					c.Println("You have selected " + ID)
+				}
+			}
+
+			if shell.Get("selected_board") == nil {
+				shell.Println("No board found!")
+			}
+		}
+
+	}
 }
 
 func listBoards(translations *translations.Translation) func(c *ishell.Context) {
